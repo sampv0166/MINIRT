706fab21114adb11ca888fa46108e5c2f14758c5
abdu <sampv0166@gmail.com>
2022-07-14 09:53:41 +0400

    ok

--- maths/misc.c
+++ maths/misc.c
@@ -5,10 +5,10 @@ double	magnitude(t_vector *vec)
 	double	ret;
 	double	sum;
 
-	vec.x = pow(vec.x, 2);
-	vec.y = pow(vec.y, 2);
-	vec.z = pow(vec.z, 2);
-	sum = vec.x + vec.y + vec.z;
+	vec->x = pow(vec->x, 2);
+	vec->y = pow(vec->y, 2);
+	vec->z = pow(vec->z, 2);
+	sum = vec->x + vec->y + vec->z;
 	ret = sqrt(sum);
 	return (ret);
 }
@@ -18,10 +18,11 @@ t_vector	*normalize(t_vector *vec)
 	t_vector	*ret;
 	double		mag;
 
+	ret = malloc (sizeof(t_vector));
 	mag = magnitude(vec);
-	ret.x = vec.x / mag;
-	ret.y = vec.y / mag;
-	ret.z = vec.z / mag;
+	ret->x = vec->x / mag;
+	ret->y = vec->y / mag;
+	ret->z = vec->z / mag;
 	return (ret);
 }
 
@@ -29,7 +30,7 @@ double	dot_product(t_vector *vec1, t_vector *vec2)
 {
 	double	ret;
 
-	ret = vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
+	ret = vec1->x * vec2->x + vec1->y * vec2->y + vec1->z * vec2->z;
 	return (ret);
 }
 
@@ -37,8 +38,9 @@ t_vector	*cross_product(t_vector *vec1, t_vector *vec2)
 {
 	t_vector	*ret;
 
-	ret.x = vec1.y * vec2.z - vec1.z * vec2.y;
-	ret.y = vec1.z * vec2.x - vec1.x * vec2.z;
-	ret.z = vec1.x * vec2.y - vec1.y * vec2.x;
-	return (ret);
+	ret = malloc (sizeof(t_vector));
+	ret->x = vec1->y * vec2->z - vec1->z * vec2->y;
+	ret->y = vec1->z * vec2->x - vec1->x * vec2->z;
+	ret->z = vec1->x * vec2->y - vec1->y * vec2->x;
+	return (ret);	
 }
