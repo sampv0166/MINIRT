73708e0395c3809a506aacfafbca2e911378ff18
Imran <imzs@hotmail.com>
2022-08-03 11:39:38 +0400

    organise files

--- render/shadow.c
+++ render/shadow.c
@@ -12,17 +12,9 @@ t_bool	is_shadowed(t_world w, t_point p)
 	v = subtract_points(w.l.pos, p);
 	distance = magnitude(v);
 	direction = normalize(v);
-	// printf("p x:%lf, y:%lf, z:%lf\n", p.x, p.y, p.z);
 	r = ray(p, direction);
-	// printf("r.origin x: %lf, y: %lf, z: %lf\n",
-	// 	r.origin.x, r.origin.y, r.origin.z);
-	// printf("r.direction x: %lf, y: %lf, z: %lf\n",
-	// 	r.direction.x, r.direction.y, r.direction.z);
 	xs = intersect_world(w, r);
-	// printf("xs count:%d, xs0: %lf, xs1: %lf, xs2: %lf, xs4: %lf\n",
-	// 	xs[0].count, xs[0].t, xs[1].t, xs[2].t, xs[3].t);
 	h = hit(xs);
-	// printf("h value: %lf\n", h.t);
 	if (h.t && h.t < distance)
 		return TRUE;
 	else
